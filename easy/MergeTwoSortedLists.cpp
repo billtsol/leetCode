c26aa922c91424f0da62e2ec6160ded150fbff41
billtsol <billtsol@protonmail.com>
2023-12-18 13:47:10 +0200

    refresh problem 6

--- easy/MergeTwoSortedLists.cpp
+++ easy/MergeTwoSortedLists.cpp
@@ -1,67 +1,79 @@
 // Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+  ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+  {
 
-        if(list1 == nullptr){
-            return list2;
-        }else if (list2 == nullptr){
-            return list1;
-        }
+    if (list1 == nullptr)
+    {
+      return list2;
+    }
+    else if (list2 == nullptr)
+    {
+      return list1;
+    }
 
-        ListNode* resultList;
+    ListNode *resultList;
 
-        if(list1->val > list2->val){
-            resultList = list2;
-            list2 = list2->next;
-        }else{
-            resultList = list1;
-            list1 = list1->next;
-        }
+    if (list1->val > list2->val)
+    {
+      resultList = list2;
+      list2 = list2->next;
+    }
+    else
+    {
+      resultList = list1;
+      list1 = list1->next;
+    }
 
-        ListNode* head = resultList;
-        while( list1 != nullptr && list2 != nullptr ){
+    ListNode *head = resultList;
+    while (list1 != nullptr && list2 != nullptr)
+    {
 
-            if(list1->val <= list2->val){
-                resultList->next = list1;
-                list1 = list1->next;
+      if (list1->val <= list2->val)
+      {
+        resultList->next = list1;
+        list1 = list1->next;
+      }
+      else if (list1->val > list2->val)
+      {
+        resultList->next = list2;
+        list2 = list2->next;
+      }
+      else
+      {
 
-            }else if (list1->val > list2->val){
-                resultList->next = list2;
-                list2 = list2->next;
-
-            }else{
-
-                resultList->next = list1;
-                list1 = list1->next;
-                resultList = resultList->next;
-
-                resultList->next = list2;
-                list2 = list2->next;
-
-            }
-
-            resultList = resultList->next;
-        }
+        resultList->next = list1;
+        list1 = list1->next;
+        resultList = resultList->next;
 
         resultList->next = list2;
+        list2 = list2->next;
+      }
 
-        if (list1 != nullptr){
-            resultList->next = list1;
-        }
-
-        return head;
+      resultList = resultList->next;
     }
-};
 
+    resultList->next = list2;
+
+    if (list1 != nullptr)
+    {
+      resultList->next = list1;
+    }
+
+    return head;
+  }
+};
 
 /*
 You are given the heads of two sorted linked lists list1 and list2.
